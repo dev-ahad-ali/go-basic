3c254dac0a510164f9655854bddeeb196278df4c
dev-ahad-ali <ahadali.dev@gmail.com>
2026-07-22 15:28:18 +0600

    reciver function demo

--- main.go
+++ main.go
@@ -2,24 +2,29 @@ package main
 
 import "fmt"
 
-func processOperation(a int, b int, op func(p int, q int)) {
-	op(a, b)
-}
-
-func call() func(x int, y int) {
-	return add
-}
-
-func add(x int, y int) {
-	z := x + y
-	fmt.Println(z)
+type User struct {
+	Name string
+	Age  int
 }
 
 func main() {
-	// higher order function
-	processOperation(2, 5, add)
-	sum := call() // function expression
-	sum(2, 8)
+	var user1 User
+
+	user1 = User{
+		Name: "Habib",
+		Age:  30,
+	}
+
+	user2 := User{ // instance or object
+		Name: "User second",
+		Age:  50,
+	}
+
+	fmt.Println("Name", user1.Name)
+	fmt.Println("Age", user1.Age)
+
+	fmt.Println("Name", user2.Name)
+	fmt.Println("Age", user2.Age)
 }
 
 func init() {
