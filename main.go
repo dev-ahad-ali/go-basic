b6e0d4470cc4082fd0bdb5253cefe7b4f3fee11f
dev-ahad-ali <ahadali.dev@gmail.com>
2026-07-24 19:41:14 +0600

    go array demo

--- main.go
+++ main.go
@@ -2,24 +2,13 @@ package main
 
 import "fmt"
 
-func processOperation(a int, b int, op func(p int, q int)) {
-	op(a, b)
-}
-
-func call() func(x int, y int) {
-	return add
-}
-
-func add(x int, y int) {
-	z := x + y
-	fmt.Println(z)
-}
-
 func main() {
-	// higher order function
-	processOperation(2, 5, add)
-	sum := call() // function expression
-	sum(2, 8)
+	var arr [2]int
+
+	arr[1] = 6
+	arr[0] = 3
+
+	fmt.Println(arr)
 }
 
 func init() {
