c943d6b1f0fa237e0e862d35a6cacd9f265cac50
Ahadul Islam <ahadali.dev@gmail.com>
2026-07-30 20:54:16 +0600

    go slice

--- main.go
+++ main.go
@@ -2,24 +2,17 @@ package main
 
 import "fmt"
 
-func processOperation(a int, b int, op func(p int, q int)) {
-	op(a, b)
-}
-
-func call() func(x int, y int) {
-	return add
-}
-
-func add(x int, y int) {
-	z := x + y
-	fmt.Println(z)
-}
-
 func main() {
-	// higher order function
-	processOperation(2, 5, add)
-	sum := call() // function expression
-	sum(2, 8)
+	arr := [6]string{"This", "is", "a", "GO", "interview", "question"}
+	fmt.Println(arr)
+
+	s := arr[1:4] // ["is", "a", "GO"]
+	fmt.Println(s)
+
+	s1 := s[1:2] // ["a"] len = 1 cap = 4
+	fmt.Println(s1)
+	fmt.Println(len(s1))
+	fmt.Println(cap(s1))
 }
 
 func init() {
